5fb8c945558881d7be98ecff45564d3d31a69d4b
MelquiadesXIII <liancarlos365@gmail.com>
2025-04-27 13:49:40 -0400

    Creacion de las Clases de Logica

---
+++ Proyecto Final 4 DPOO/src/Logica/CapituloLibro.java
@@ -0,0 +1,5 @@
+package Logica;
+
+public class CapituloLibro extends ResultadoInvestigativo{
+
+}
---
+++ Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
@@ -0,0 +1,9 @@
+package Principal;
+
+public class Inicializadora {
+
+	public static void main(String[] args) {
+
+	}
+
+}
