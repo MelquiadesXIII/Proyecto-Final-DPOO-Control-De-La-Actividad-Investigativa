2a6205ddd662b67bd32321c8d0ab75d66b14e42e
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-11 21:27:28 -0400

    Update Maestria.java

--- Proyecto Final 4 DPOO/src/Logica/Maestria.java
+++ Proyecto Final 4 DPOO/src/Logica/Maestria.java
@@ -6,18 +6,75 @@ public class Maestria {
 
 	private ArrayList<CursoPosgrado> cursos;
 	private ArrayList<Docente> matriculados;
-	
-	
-	
+
+
+
+	//Constructor
+	public Maestria() 
+	{
+		cursos = new ArrayList<CursoPosgrado>();
+		matriculados = new ArrayList<Docente>();
+	}
+
+
+
+	//Getters
+	public ArrayList<CursoPosgrado> getCursos() 
+	{
+		return cursos;
+	}
+
+	public ArrayList<Docente> getMatriculados() 
+	{
+		return matriculados;
+	}
+
+
+
+	//Metodos
+	public void agregarMatriculado(Docente matriculado)
+	{
+		if(matriculado == null)
+			throw new NullPointerException("El docente matriculado no puede tener valor null");
+
+		matriculados.add(matriculado);
+	}
+
+	public void removerMatriculado(Docente matriculado)
+	{
+		if(matriculado == null)
+			throw new NullPointerException("El docente matriculado no puede tener valor null");
+
+		if(!matriculados.contains(matriculado))
+			throw new IllegalArgumentException("La lista de matriculados no contiene al docente");
+
+		matriculados.remove(matriculado);
+
+		for(CursoPosgrado c: cursos){
+
+			if(c.getParticipantes().contains(matriculado))
+				c.getParticipantes().remove(matriculado);
+		}
+
+	}
+
 	//Implementar
 	public boolean recibirVistoBueno(Docente d){
-		
+
 		return true;
 	}
-	
+
 	//Implementar
 	public int creditosObtenidosCursosRecibidos(Docente d){
-		
+
 		return 0;
 	}
+
+
+
+
+
+
+
+
 }
