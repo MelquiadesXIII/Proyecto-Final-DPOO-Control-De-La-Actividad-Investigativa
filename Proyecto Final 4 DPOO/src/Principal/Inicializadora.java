01475b5e58b67738b47017f174b31296a89061c8
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-12 23:48:11 -0400

    Update Inicializadora.java

--- Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
+++ Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
@@ -1,12 +1,14 @@
 package Principal;
 
+import javax.swing.JOptionPane;
+
 import Logica.Vicedecanato;
 
 public class Inicializadora {
 
 	public static void main(String[] args) {
 		
-		
+		JOptionPane.showMessageDialog(null, "Error", "Error cojone", JOptionPane.ERROR_MESSAGE);
 		
 	}
 
