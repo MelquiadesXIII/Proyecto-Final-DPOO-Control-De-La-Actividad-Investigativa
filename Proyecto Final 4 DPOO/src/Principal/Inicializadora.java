98f6bfb48e6c327500a53feb0fed6ce593ad0b19
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-17 19:57:11 -0400

    Update Inicializadora.java

--- Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
+++ Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
@@ -3,31 +3,52 @@ package Principal;
 import java.util.ArrayList;
 
 import javax.swing.*;
+
+import Interfaz.Login;
 import Logica.CategoriaCientifica;
 import Logica.CategoriaDocente;
 import Logica.CursoPosgrado;
 import Logica.Docente;
 
-public class Inicializadora {
+public class Inicializadora{
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> obj = new ArrayList<>();
-		
-		obj.add("Programar");
-		obj.add("Ir a Varadero");
-		
-		Docente d1 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
-		Docente d2 = new Docente("Ryan", "Negrete", CategoriaCientifica.MASTER, CategoriaDocente.PROFESOR_AUXILIAR);
-		Docente d3 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
+		SwingUtilities.invokeLater(new Runnable() {
+			
+	        public void run() {
+	        	
+	        	ArrayList<String> obj = new ArrayList<>();
+				
+				obj.add("Programar");
+				obj.add("Ir a Varadero");
+				
+				Docente d1 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
+				Docente d2 = new Docente("Ryan", "Negrete", CategoriaCientifica.MASTER, CategoriaDocente.PROFESOR_AUXILIAR);
+				Docente d3 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
 
+				
+				CursoPosgrado c = new CursoPosgrado("Mujeres", obj, 5, d1);
+				c.agregarParticipante(d2);
+				c.emitirNota(d1, d2, 2);
+				
+				if(d1.equals(d1))
+					System.out.println("hola");
+				
+	            try {
+	            	
+	            	new Login();
+	            	
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    });
 		
-		CursoPosgrado c = new CursoPosgrado("Mujeres", obj, 5, d1);
-		c.agregarParticipante(d2);
-		c.emitirNota(d1, d2, 2);
 		
-		if(d1.equals(d1))
-			System.out.println("hola");
+		
+		
+		
 	}
 
 }
