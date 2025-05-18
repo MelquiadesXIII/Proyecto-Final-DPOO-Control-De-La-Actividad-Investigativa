ce216aaa85c6c33b4c4d65d8249731dfa130953b
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-17 20:15:30 -0400

    Update Login.java

--- Proyecto Final 4 DPOO/src/Interfaz/Login.java
+++ Proyecto Final 4 DPOO/src/Interfaz/Login.java
@@ -1,6 +1,7 @@
 package Interfaz;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -14,15 +15,17 @@ public class Login extends JFrame {
 	
 	public Login() {
 		
-		setVisible(true);
+		setTitle("Vicedecanato: Iniciar sesi\u00F3n o registrarse");
 		setResizable(false);
+		setSize(1080, 720);;
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setBackground(Color.BLACK);
+		
+		setVisible(true);
 	}
 
 }
