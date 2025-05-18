94cf82d2f46e11a146b548a3ffd100ec7030712a
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-17 23:02:30 -0400

    Update PonenciaEvento.java

--- Proyecto Final 4 DPOO/src/Logica/PonenciaEvento.java
+++ Proyecto Final 4 DPOO/src/Logica/PonenciaEvento.java
@@ -1,10 +1,16 @@
 package Logica;
 
+import java.time.LocalDate;
+import java.time.Year;
+
+import Excepciones.CadenaNoValidaException;
+import Excepciones.ValorNoValidoException;
+
 public class PonenciaEvento extends ResultadoInvestigativo{
 
 	private String nombre;
 	private String anio;
-	private String fecha;
+	private LocalDate fecha;
 	private String lugar;
 	private String ISBN;
 	private static int puntos = 1;
@@ -12,7 +18,15 @@ public class PonenciaEvento extends ResultadoInvestigativo{
 	
 
 	//Constructor
-
+	public PonenciaEvento(String nombre, LocalDate fecha, String lugar, String iSBN) {
+		
+		super();
+		
+		setNombre(nombre);
+		setFecha(fecha);
+		setISBN(iSBN);
+		setLugar(lugar);
+	}
 
 
 	//Getters
@@ -28,7 +42,7 @@ public class PonenciaEvento extends ResultadoInvestigativo{
 		return lugar;
 	}
 
-	public String getFecha() {
+	public LocalDate getFecha() {
 		return fecha;
 	}
 
@@ -43,8 +57,55 @@ public class PonenciaEvento extends ResultadoInvestigativo{
 
 
 	//Setters
+	public void setFecha(LocalDate fecha) {
+		// LocalDate fechaLocal = fechaSeleccionada.toInstant()
+		//            .atZone(ZoneId.systemDefault())
+		//            .toLocalDate();  -> esto se va a usar en la interfaz para convertir la fecha seleccionada en un LocalDate
+		int anyoMaximo = Year.now().getValue() - 1;
+		int anyoMinimo = 1900;
 
+		if(fecha.getYear() < anyoMinimo)
+			throw new ValorNoValidoException("La fecha seleccionada no puede ser anterior a 1900");
+		
+		if(fecha.getYear() > anyoMaximo)
+			throw new ValorNoValidoException("La fecha seleccionada no puede ser después del año " +anyoMaximo);
 
+		this.fecha = fecha;
+		anio = Integer.toString(fecha.getYear()); //Inicializado aqui para garantizar la coherencia
+	}
+	
+	public void setNombre(String nombre) {
+		
+		if(nombre.trim().isEmpty())
+			throw new CadenaNoValidaException("El nombre de la ponencia no puede estar vacio");
+
+		if(!nombre.matches("^[\\p{L}\\s]+$"))
+			throw new CadenaNoValidaException("El nombre de la ponencia solo puede tener letras y espacios");	
+		
+		this.nombre = nombre;
+	}
+
+	public void setLugar(String lugar) {
+		
+		if(lugar.trim().isEmpty())
+			throw new CadenaNoValidaException("El nombre del lugar no puede estar vacio");
+
+		if(!lugar.matches("^[\\p{L}\\s]+$"))
+			throw new CadenaNoValidaException("El nombre del lugar solo puede tener letras y espacios");
+		
+		this.lugar = lugar;
+	}
+
+	public void setISBN(String ISBN) {
+		
+		if(ISBN.length() < 13 || ISBN.length() > 13)
+			throw new CadenaNoValidaException("El ISBN solo puede tener una longitud de 13 dígitos, la longitud de lo que usted ingresó es " +ISBN.length());
+			
+		if(!ISBN.matches("^\\d+$"))
+			throw new CadenaNoValidaException("El ISBN solo puede estar conformado por números");
+			
+		this.ISBN = ISBN;
+	}
 
 	//Metodos
 	@Override
@@ -64,4 +125,5 @@ public class PonenciaEvento extends ResultadoInvestigativo{
 
 		return puntos;
 	}
+
 }
