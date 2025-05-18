0dd65cd94b07efcd95536a0ea718b81626121a24
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-17 23:36:10 -0400

    Update LineaInvestigacion.java

--- Proyecto Final 4 DPOO/src/Logica/LineaInvestigacion.java
+++ Proyecto Final 4 DPOO/src/Logica/LineaInvestigacion.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class LineaInvestigacion {
 
 	private String nombre;
-	private ArrayList<ResultadoInvestigativo> resultados;
 	private ArrayList<Investigador> investigadores;
 
 
 
 	//Constructor
+	public LineaInvestigacion(String nombre) {
+
+		setNombre(nombre);
+		investigadores = new ArrayList<Investigador>();
+	}
 
 
 
@@ -19,14 +23,30 @@ public class LineaInvestigacion {
 		return nombre;
 	}
 
+	public ArrayList<Investigador> getInvestigadores() {
+		return investigadores;
+	}
 
+	
 
+	//Setters
+	public void setNombre(String nombre) {
+		
+		this.nombre = nombre;
+	}
+	
+	
+	
 	//Metodos
 	@Override
 	public boolean equals(Object o) {
 		return o != null &&
 				(this == o ||
-						(o instanceof LineaInvestigacion &&
-								nombre.equals(((LineaInvestigacion)o).getNombre())));
+				(o instanceof LineaInvestigacion &&
+						nombre.equals(((LineaInvestigacion)o).getNombre())));
 	}
+
+
+
+	
 }
