99f20ac7d3dd39d5c721727ef632af1d5b343847
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-19 17:01:08 -0400

    Update Departamento.java

--- Proyecto Final 4 DPOO/src/Logica/Departamento.java
+++ Proyecto Final 4 DPOO/src/Logica/Departamento.java
@@ -3,6 +3,7 @@ package Logica;
 import java.util.ArrayList;
 
 import Excepciones.CadenaNoValidaException;
+import Excepciones.DuplicacionException;
 import Excepciones.ListaVaciaException;
 import Excepciones.NoExistenciaException;
 
@@ -23,10 +24,12 @@ public class Departamento {
 
 		docentes = new ArrayList<Docente>();
 		estudiantes = new ArrayList<Estudiante>();
+		maestrias = new ArrayList<Maestria>();
+		lineasInvestigacion = new ArrayList<LineaInvestigacion>();
 	}
 
-	
-	
+
+
 	//Getters
 	public String getNombre() {
 		return nombre;
@@ -40,8 +43,16 @@ public class Departamento {
 		return estudiantes;
 	}
 
+	public ArrayList<Maestria> getMaestrias() {
+		return maestrias;
+	}
 
-	
+	public ArrayList<LineaInvestigacion> getLineasInvestigacion() {
+		return lineasInvestigacion;
+	}
+
+
+
 	//Setters
 	public void setNombre(String nombre) 
 	{
@@ -61,8 +72,8 @@ public class Departamento {
 	public boolean equals(Object o) {
 		return o != null &&
 				(this == o ||
-						(o instanceof Departamento &&
-								nombre.equals(((Departamento)o).getNombre())));
+				(o instanceof Departamento &&
+						nombre.equals(((Departamento)o).getNombre())));
 	}
 
 	public void removerDocente(Docente d) {
@@ -81,10 +92,93 @@ public class Departamento {
 				m.removerMatriculado(d);
 		}
 
-		for(LineaInvestigacion l: lineasInvestigacion){ //A implemementar con los investigadores
+		for(LineaInvestigacion l: lineasInvestigacion){
 
 			if(l.getInvestigadores().contains(d))
 				l.removerInvestigador(d);
 		}
+	}
+
+	public void agregarDocente(Docente d){
+
+		if(d == null)
+			throw new NullPointerException("El docente matriculado no puede tener valor null");
+
+		if(docentes.contains(d))
+			throw new DuplicacionException("El profesor no se puede agregar porque ya se encuentra presente en el departamento");
+
+		docentes.add(d);
+	}
+
+	public void removerEstudiante(Estudiante e) {
+
+		if(estudiantes.isEmpty())
+			throw new ListaVaciaException("La lista de la que desea remover al estudiante esta vacia");
+
+		if(!estudiantes.contains(e))
+			throw new NoExistenciaException("El estudiante que desea remover no se encuentra registrado en el departamento");
+
+		estudiantes.remove(e);
+
+		for(LineaInvestigacion l: lineasInvestigacion){
+
+			if(l.getInvestigadores().contains(e))
+				l.removerInvestigador(e);
+		}
+	}
+
+	public void agregarEstudiante(Estudiante e){
+
+		if(e == null)
+			throw new NullPointerException("El estudiante no puede tener valor null");
+
+		if(estudiantes.contains(e))
+			throw new DuplicacionException("El estudiante no se puede agregar porque ya se encuentra presente en el departamento");
+
+		estudiantes.add(e);
+	}
+
+	public void agregarMaestria(Maestria m) {
+
+		if(m == null)
+			throw new NullPointerException("La maestría no puede tener valor null");
+
+		if(maestrias.contains(m))
+			throw new DuplicacionException("La maestría no se puede agregar porque ya se encuentra registrada en el vicedecanato");
+
+		maestrias.add(m);
+	}
+
+	public void agregarLineaInvestigacion(LineaInvestigacion l) {
+
+		if(l == null)
+			throw new NullPointerException("La línea de investigación no puede tener valor null");
+
+		if(lineasInvestigacion.contains(l))
+			throw new DuplicacionException("La línea de investigación no se puede agregar porque ya se encuentra registrada en el vicedecanato");
+
+		lineasInvestigacion.add(l);
+	}
+
+	public void removerLineaInvestigacion(LineaInvestigacion l) {
+
+		if(lineasInvestigacion.isEmpty())
+			throw new ListaVaciaException("La lista de la que desea remover la línea de investigación está vacía");
+
+		if(!lineasInvestigacion.contains(l))
+			throw new NoExistenciaException("La línea de investigación que desea remover no se encuentra registrada en el departamento");
+
+		lineasInvestigacion.remove(l);
+	}
+
+	public void removerMaestria(Maestria m) {
+
+		if(maestrias.isEmpty())
+			throw new ListaVaciaException("La lista de la que desea remover la maestría está vacía");
+
+		if(!maestrias.contains(m))
+			throw new NoExistenciaException("La maestría que desea remover no se encuentra registrada en el departamento");
+
+		maestrias.remove(m);
 	}
 }
