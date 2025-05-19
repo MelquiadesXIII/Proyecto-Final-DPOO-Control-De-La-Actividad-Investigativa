dd728259610d6769ffc238c03430736b65ce58b5
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-19 17:22:08 -0400

    Update CategoriaCientifica.java

--- Proyecto Final 4 DPOO/src/Logica/CategoriaCientifica.java
+++ Proyecto Final 4 DPOO/src/Logica/CategoriaCientifica.java
@@ -2,7 +2,17 @@ package Logica;
 
 public enum CategoriaCientifica {
 
-	MASTER,
-	DOCTOR,
-	NINGUNA;
+	MASTER("Master"),
+	DOCTOR("Doctor"),
+	NINGUNA("Ninguna");
+
+	private final String categoria;
+
+	CategoriaCientifica(String categoria){
+		this.categoria = categoria;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
 }
