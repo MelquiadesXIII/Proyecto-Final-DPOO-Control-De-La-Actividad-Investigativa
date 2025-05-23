c74243bc8941f39a327e9a3bdce0e7a3b73e7979
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-23 09:36:21 -0400

    Update Inicializadora.java

--- Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
+++ Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
@@ -14,75 +14,12 @@ public class Inicializadora{
 	public static void main(String[] args) {
 
 		SwingUtilities.invokeLater(new Runnable() {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-			
-	        public void run() {
-	        	
-	        	ArrayList<String> obj = new ArrayList<>();
-				
-=======
-
-			public void run() {
-
-				ArrayList<String> obj = new ArrayList<>();
-
->>>>>>> main
-				obj.add("Programar");
-				obj.add("Ir a Varadero");
-
-				Docente d1 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
-				Docente d2 = new Docente("Ryan", "Negrete", CategoriaCientifica.MASTER, CategoriaDocente.PROFESOR_AUXILIAR);
-				Docente d3 = new Docente("Manuel", "Alfonso", CategoriaCientifica.DOCTOR, CategoriaDocente.PROFESOR_TITULAR);
-
-
-				CursoPosgrado c = new CursoPosgrado("Mujeres", obj, 5, d1);
-				c.agregarParticipante(d2);
-				c.emitirNota(d1, d2, 2);
-<<<<<<< HEAD
-				
-				if(d1.equals(d1))
-					System.out.println("hola");
-				
-	            try {
-	            	
-	            	new Login();
-	            	
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    });
-		
-		
-		
-		
-		
-=======
-
 			public void run() {
 				
 				
 
+
 			}
 		});
-
->>>>>>> Stashed changes
-	}
-=======
->>>>>>> main
-
-				if(d1.equals(d1))
-
-				try {
-
-					new InterfazPrincipal();
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-
 	}
 }
