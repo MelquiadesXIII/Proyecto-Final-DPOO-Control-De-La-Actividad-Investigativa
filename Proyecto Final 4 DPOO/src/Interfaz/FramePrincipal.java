454466ea624e7c752dcf8ca1169105f1eef7e48e
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-24 23:48:21 -0400

    Create FramePrincipal.java

---
+++ Proyecto Final 4 DPOO/src/Interfaz/FramePrincipal.java
@@ -0,0 +1,22 @@
+package Interfaz;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class FramePrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public FramePrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
