04a6247c7f852ae9d40cf96d02ba700e5139cc32
schnellerMarlon <llerandimarlon05@gmail.com>
2025-05-26 17:14:43 -0400

    update cardLayout y listas en la nueva interfaz

--- Proyecto Final 4 DPOO/src/Interfaz/VentanaPrincipal.java
+++ Proyecto Final 4 DPOO/src/Interfaz/VentanaPrincipal.java
@@ -1,9 +1,11 @@
 package Interfaz;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 import Interfaz.MensajeDialog.Tipo;
 import Logica.*;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -13,17 +15,27 @@ import java.awt.event.ActionEvent;
 public class VentanaPrincipal extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
-	JPanel panelNavegacion;
-	JPanel panelPrincipal;
-	JButton botonDepartamentos;
-	JPanel panelBotones;
-	JButton botonDocentes;
-	JButton botonEstudiantes;
-	JButton botonAyuda;
-	JButton botonSalir;
-	JButton botonInicio;
-	CardLayout cardLayout;
-	Vicedecanato vicedecanato;
+	private JPanel panelNavegacion;
+	private JPanel panelPrincipal;
+	private JButton botonDepartamentos;
+	private JPanel panelBotones;
+	private JButton botonDocentes;
+	private JButton botonEstudiantes;
+	private JButton botonAyuda;
+	private JButton botonSalir;
+	private JButton botonInicio;
+	private CardLayout cardLayout;
+	private Vicedecanato vicedecanato;
+	private JPanel panelDepartamentos;
+	private JPanel panelEstudiantes;
+	private JPanel panelDocentes;
+	private JPanel panelInicio;
+	private JList<String> listaDepartamentos;
+	private DefaultListModel<String> modeloDepartamentos;
+	private JList<String> listaDocentes;
+	private DefaultListModel<String> modeloDocentes;
+	private JList<String> listaEstudiantes;
+	private DefaultListModel<String> modeloEstudiantes;
 	
 	public VentanaPrincipal(Vicedecanato vicedecanato){
 		
@@ -31,6 +43,11 @@ public class VentanaPrincipal extends JFrame{
 		inicializarConfiguracionUI();
 		crearPanelPrincipal();
 		crearPanelNavegacion();
+		crearPaneles();
+		crearTablaDocentes();
+		crearTablaDepartamentos();
+		crearTablaEstudiantes();
+		crearBotones();
 	}
 	
 	private void inicializarConfiguracionUI(){
@@ -71,12 +88,36 @@ public class VentanaPrincipal extends JFrame{
 		panelNavegacion.add(panelBotones);
 		
 		botonInicio = crearBoton("Inicio");
+		botonInicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panelPrincipal.getLayout());
+				cl.show(panelPrincipal, "panelInicio");
+			}
+		});
 		
 		botonDepartamentos = crearBoton("Departamentos");
+		botonDepartamentos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panelPrincipal.getLayout());
+				cl.show(panelPrincipal, "panelDepartamentos");
+			}
+		});
 		
 		botonDocentes = crearBoton("Docentes");
+		botonDocentes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panelPrincipal.getLayout());
+				cl.show(panelPrincipal, "panelDocentes");
+			}
+		});
 		
 		botonEstudiantes = crearBoton("Estudiantes");
+		botonEstudiantes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panelPrincipal.getLayout());
+				cl.show(panelPrincipal, "panelEstudiantes");
+			}
+		});
 		
 		botonAyuda = crearBoton("Ayuda");
 		
@@ -129,6 +170,114 @@ public class VentanaPrincipal extends JFrame{
 		panelPrincipal = new JPanel(cardLayout);
 		panelPrincipal.setBackground(Color.DARK_GRAY);
 		getContentPane().add(panelPrincipal, BorderLayout.CENTER);
+	}
+
+	private void crearPaneles(){
+		
+		panelInicio = new JPanel();
+		panelPrincipal.add(panelInicio, "panelInicio");
+		panelInicio.setBackground(Color.DARK_GRAY);
+
+		panelDocentes = new JPanel();
+		panelPrincipal.add(panelDocentes, "panelDocentes");
+		panelDocentes.setBackground(Color.DARK_GRAY);
+		
+		panelEstudiantes = new JPanel();
+		panelPrincipal.add(panelEstudiantes, "panelEstudiantes");
+		panelEstudiantes.setBackground(Color.DARK_GRAY);
+
+		panelDepartamentos = new JPanel();
+		panelPrincipal.add(panelDepartamentos, "panelDepartamentos");
+		panelDepartamentos.setBackground(Color.DARK_GRAY);
+		panelInicio.setLayout(null);
+		
+		JLabel lblBienvenida = new JLabel("Bienvenido!");
+		lblBienvenida.setBounds(0, 0, 794, 465);
+		lblBienvenida.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
+		lblBienvenida.setHorizontalAlignment(SwingConstants.CENTER);
+		panelInicio.add(lblBienvenida);
+	}
+	
+	private void crearTablaDepartamentos(){
+		
+		modeloDepartamentos = new DefaultListModel<>();
+
+		for (Departamento departamento : vicedecanato.getDepartamentos()) {
+			modeloDepartamentos.addElement(departamento.getNombre());
+		}
+		panelDepartamentos.setLayout(null);
+
+		listaDepartamentos = new JList<>(modeloDepartamentos);
+		listaDepartamentos.setForeground(Color.WHITE);
+		listaDepartamentos.setBackground(Color.DARK_GRAY);
+		listaDepartamentos.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		JScrollPane scrollDepartamentos = new JScrollPane(listaDepartamentos);
+		scrollDepartamentos.setBounds(0, 1, 650, 685);
+		panelDepartamentos.add(scrollDepartamentos);
+	}
+	
+	private void crearTablaEstudiantes(){
+		
+		modeloEstudiantes = new DefaultListModel<>();
+
+		for (Estudiante estudiante : vicedecanato.getEstudiantes()) {
+			modeloEstudiantes.addElement(estudiante.getNombre() + " " + estudiante.getApellidos());
+		}
+		panelEstudiantes.setLayout(null);
+
+		listaEstudiantes = new JList<>(modeloEstudiantes);
+		listaEstudiantes.setForeground(Color.WHITE);
+		listaEstudiantes.setBackground(Color.DARK_GRAY);
+		listaEstudiantes.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		JScrollPane scrollEstudiantes = new JScrollPane(listaEstudiantes);
+		scrollEstudiantes.setBounds(0, 1, 650, 685);
+
+		panelEstudiantes.add(scrollEstudiantes);
+	}
+	
+	private void crearTablaDocentes(){
+		
+		modeloDocentes = new DefaultListModel<>();
+
+		for (Docente docente : vicedecanato.getDocentes()) {
+			modeloDocentes.addElement(docente.getNombre() + " " + docente.getApellidos());
+		}
+		panelDocentes.setLayout(null);
+
+		listaDocentes = new JList<>(modeloDocentes);
+		listaDocentes.setForeground(Color.WHITE);
+		listaDocentes.setBackground(Color.DARK_GRAY);
+		listaDocentes.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		JScrollPane scrollDocentes = new JScrollPane(listaDocentes);
+		scrollDocentes.setBounds(0, 1, 650, 685);
+		panelDocentes.add(scrollDocentes);
+	}
+	
+	private void crearBotones(){
+		
+		JButton btnCrearEst = new JButton("Crear");
+		btnCrearEst.setBounds(660, 13, 140, 25);
+		panelEstudiantes.add(btnCrearEst);
+		
+		JButton btnEliminarEst = new JButton("Eliminar");
+		btnEliminarEst.setBounds(660, 60, 140, 25);
+		panelEstudiantes.add(btnEliminarEst);
+		
+		JButton btnCrearDoc = new JButton("Crear");
+		btnCrearDoc.setBounds(660, 13, 140, 25);
+		panelDocentes.add(btnCrearDoc);
+
+		JButton btnEliminarDoc = new JButton("Eliminar");
+		btnEliminarDoc.setBounds(660, 60, 140, 25);
+		panelDocentes.add(btnEliminarDoc);
+		
+		JButton btnCrearDep = new JButton("Crear");
+		btnCrearDep.setBounds(660, 13, 140, 25);
+		panelDepartamentos.add(btnCrearDep);
+
+		JButton btnEliminarDep = new JButton("Eliminar");
+		btnEliminarDep.setBounds(660, 60, 140, 25);
+		panelDepartamentos.add(btnEliminarDep);
 	}
 	
 	
