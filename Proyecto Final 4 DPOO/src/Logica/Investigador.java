44a0cab53693e07a4e4c1ea11c0cf3feff5f4f5a
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-30 23:34:09 -0400

    Update Investigador.java

--- Proyecto Final 4 DPOO/src/Logica/Investigador.java
+++ Proyecto Final 4 DPOO/src/Logica/Investigador.java
@@ -1,5 +1,6 @@
 package Logica;
 
+import Excepciones.CadenaNoValidaException;
 import Excepciones.DuplicacionException;
 import Excepciones.ListaVaciaException;
 import Excepciones.NoExistenciaException;
@@ -8,13 +9,17 @@ import java.util.ArrayList;
 
 public abstract class Investigador {
 
+	protected String nombre;
+	protected String apellidos;
 	protected ArrayList<ResultadoInvestigativo> resultados;
 
 
 	
 	//Constructor
-	public Investigador() {
+	public Investigador(String nombre, String apellidos) {
 
+		setNombre(nombre);
+		setApellidos(apellidos);
 		resultados = new ArrayList<ResultadoInvestigativo>();
 
 	}
@@ -24,8 +29,40 @@ public abstract class Investigador {
 		return resultados;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
 
+	public String getApellidos() {
+		return apellidos;
+	}
 
+	//Setters
+	public void setNombre(String nombre) 
+	{
+		if(nombre.trim().isEmpty())
+			throw new CadenaNoValidaException("El nombre del docente no puede estar vacío");
+		
+		if(!nombre.matches("^[\\p{L}\\s]+$"))
+			throw new CadenaNoValidaException("El nombre del docente solo puede tener letras y espacios");	
+
+		this.nombre = nombre;
+	}
+
+	public void setApellidos(String apellidos) 
+	{
+		if(apellidos.trim().isEmpty())
+			throw new CadenaNoValidaException("El nombre del docente no puede estar vacío");
+
+		if(!apellidos.matches("^[^0-9]*$"))
+			throw new CadenaNoValidaException("El nombre del docente no puede tener numeros");
+
+		if(!apellidos.matches("^[\\p{L}\\s]+$"))
+			throw new CadenaNoValidaException("El nombre del docente no puede tener caracteres especiales o símbolos");	
+
+		this.apellidos = apellidos;
+	}
+	
 	//Metodos
 	public void agregarResultado(ResultadoInvestigativo r){
 
