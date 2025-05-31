344fbd567536548bfc493b310c75841e246bc8cc
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-30 23:34:14 -0400

    Update Vicedecanato.java

--- Proyecto Final 4 DPOO/src/Logica/Vicedecanato.java
+++ Proyecto Final 4 DPOO/src/Logica/Vicedecanato.java
@@ -1,6 +1,9 @@
 package Logica;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import Excepciones.DuplicacionException;
 import Excepciones.ListaVaciaException;
 import Excepciones.NoExistenciaException;
@@ -10,20 +13,28 @@ public class Vicedecanato {
 	private ArrayList<Docente> docentes;
 	private ArrayList<Estudiante> estudiantes;
 	private ArrayList<Departamento> departamentos;
-	
-	
-	
+	private static Vicedecanato vicedecanato;
+
+
+
 	//Constructor
-	public Vicedecanato()
+	private Vicedecanato()
 	{
 		docentes = new ArrayList<Docente>();
 		estudiantes = new ArrayList<Estudiante>();
 		departamentos = new ArrayList<Departamento>();
 	}
 
-	
-	
+
+
 	//Getters
+	public static Vicedecanato getVicedecanato(){
+
+		if(vicedecanato == null)
+			vicedecanato = new Vicedecanato();
+
+		return vicedecanato; 
+	}
 	public ArrayList<Docente> getDocentes() 
 	{
 		return docentes;
@@ -38,22 +49,22 @@ public class Vicedecanato {
 	{
 		return departamentos;
 	}
-	
-	
-	
+
+
+
 	//Metodos
 	public void agregarDocente(Docente d)
 	{
 		if(docentes.contains(d))
 			throw new DuplicacionException("El docente ya se encuentra en el vicedecanato");
-		
+
 		docentes.add(d);
 	}
 
 	public void agregarEstudiante(Estudiante e)
 	{
 		if(estudiantes.contains(e))
-			throw new DuplicacionException("El docente ya se encuentra en el vicedecanato");
+			throw new DuplicacionException("El estudiante ya se encuentra en el vicedecanato");
 
 		estudiantes.add(e);
 	}
@@ -61,7 +72,7 @@ public class Vicedecanato {
 	public void agregarDepartamento(Departamento d)
 	{
 		if(departamentos.contains(d))
-			throw new DuplicacionException("El docente ya se encuentra en el vicedecanato");
+			throw new DuplicacionException("El departamento ya se encuentra en el vicedecanato");
 
 		departamentos.add(d);
 	}
@@ -113,9 +124,85 @@ public class Vicedecanato {
 
 	public boolean darVistoBuenoMaestria(Maestria m, Docente d) {
 
-		if(!m.getMatriculados().contains(d))
+		if(!m.contieneMatriculado(d))
 			throw new NoExistenciaException("El docente no se encuentra matriculado en la maestría");
 
-		return d.creditosObtenidosCursosRecibidos() == m.getCreditosTotalesCursos();
+		return d.getCreditosObtenidosCursosRecibidos() >= m.getCreditosTotalesCursos();
+	}
+
+	public void generarDatosReporte1(ArrayList<Investigador> ranking, Departamento depto){
+
+		ranking.clear();
+
+		for(LineaInvestigacion l: depto.getLineasInvestigacion()){
+			for(Investigador i: l.getInvestigadores()){
+
+				ranking.add(i);
+
+			}
+		}
+
+		Collections.sort(ranking, new Comparator<Investigador>() {
+			@Override
+			public int compare(Investigador i1, Investigador i2) {
+				return Integer.compare(i2.calcularPuntajeInvestigativo(), i1.calcularPuntajeInvestigativo());
+
+			}
+		});
+	}
+
+	public void crearDocente(String nombre, String apellidos, CategoriaCientifica catCientifica, CategoriaDocente catDocente, Departamento depto){
+
+		Docente d = new Docente(nombre, apellidos, catCientifica, catDocente);
+
+		agregarDocente(d);
+		depto.agregarDocente(d);
+	}
+
+	public void crearEstudiante(String nombre, String apellidos, String grupo, Departamento depto){
+
+		Estudiante e = new Estudiante(nombre, apellidos, grupo);
+
+		agregarEstudiante(e);
+		depto.agregarEstudiante(e);
+	}
+
+	public void crearDepartamento(String nombre){
+
+		Departamento depto = new Departamento(nombre);
+
+		agregarDepartamento(depto);
+	}
+
+	public boolean solicitudValidaIngresoDocenteADepartamento(Docente d){
+
+		boolean noContenido = true;
+
+		int i = 0;
+		while(i < departamentos.size() && noContenido){
+
+			if(departamentos.get(i).contieneDocente(d))
+				noContenido = false;
+
+			i++;
+		}
+
+		return noContenido;
+	}
+
+	public boolean solicitudValidaIngresoEstudianteADepartamento(Estudiante e){
+
+		boolean noContenido = true;
+
+		int i = 0;
+		while(i < departamentos.size() && noContenido){
+
+			if(departamentos.get(i).contieneEstudiante(e))
+				noContenido = false;
+
+			i++;
+		}
+
+		return noContenido;
 	}
 }
