5f43c11b9baa56fd7d1440c3b111799c824e11bb
MelquiadesXIII <liancarlos365@gmail.com>
2025-05-30 23:34:06 -0400

    Update Estudiante.java

--- Proyecto Final 4 DPOO/src/Logica/Estudiante.java
+++ Proyecto Final 4 DPOO/src/Logica/Estudiante.java
@@ -4,8 +4,6 @@ import Excepciones.CadenaNoValidaException;
 
 public class Estudiante extends Investigador{
 
-	private String nombre;
-	private String apellidos;
 	private String grupo;
 	
 	
@@ -13,26 +11,14 @@ public class Estudiante extends Investigador{
 	//Constructor
 	public Estudiante(String nombre, String apellidos, String grupo) {
 		
-		super();
-
-		setNombre(nombre);
-		setApellidos(apellidos);
+		super(nombre, apellidos);
+		
 		setGrupo(grupo);
 	}
 	
 	
 	
 	//Getters
-	public String getNombre() 
-	{
-		return nombre;
-	}
-	
-	public String getApellidos()
-	{
-		return apellidos;
-	}
-
 	public String getGrupo() 
 	{
 		return grupo;
@@ -41,28 +27,6 @@ public class Estudiante extends Investigador{
 
 	
 	//Setters
-	public void setNombre(String nombre) 
-	{
-		if(nombre.trim().isEmpty())
-			throw new CadenaNoValidaException("El nombre del estudiante no puede estar vacío");
-
-		if(!nombre.matches("^[\\p{L}\\s]+$"))
-			throw new CadenaNoValidaException("El nombre del estudiante solo puede tener letras y espacios");	
-
-		this.nombre = nombre;
-	}
-
-	public void setApellidos(String apellidos) 
-	{
-		if(apellidos.trim().isEmpty())
-			throw new CadenaNoValidaException("El nombre del estudiante no puede estar vacío");
-
-		if(!apellidos.matches("^[\\p{L}\\s]+$"))
-			throw new CadenaNoValidaException("El nombre del estudiante solo puede tener letras y espacios");	
-
-		this.apellidos = apellidos;
-	}
-
 	public void setGrupo(String grupo)
 	{
 		if(grupo.trim().isEmpty())
