fb5def976e15e6b87cb7620aaa123efb8ce08880
MelquiadesXIII <liancarlos365@gmail.com>
2025-06-18 19:15:53 -0400

    Update Usuario.java

--- Proyecto Final 4 DPOO/src/Login/Usuario.java
+++ Proyecto Final 4 DPOO/src/Login/Usuario.java
@@ -1,5 +1,7 @@
 package Login;
 
+import Excepciones.CadenaNoValidaException;
+
 public class Usuario {
 
 	private String nombre;
@@ -14,13 +16,30 @@ public class Usuario {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
+		
+		if(nombre.trim().isEmpty())
+			throw new CadenaNoValidaException("El nombre del usuario no puede estar vacío");
+
+		if(!nombre.matches("^[\\p{L}\\s]+$"))
+			throw new CadenaNoValidaException("El nombre de usuario solo puede tener letras y espacios");	
+
 		this.nombre = nombre;
 	}
 	public String getContrasenia() {
 		return contrasenia;
 	}
 	public void setContrasenia(String contrasenia) {
+		
+		if(contrasenia.trim().isEmpty())
+			throw new CadenaNoValidaException("La contraseña no puede estar vacío");
+		
 		this.contrasenia = contrasenia;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		
+		return o != null && (this == o || nombre.equals(((Usuario)o).getNombre()) || contrasenia.equals(((Usuario)o).getContrasenia()));
 	}
 	
 }
