ff6549381cbaf058c7a72271e96193f4b71a60fc
schnellerMarlon <llerandimarlon05@gmail.com>
2025-06-21 11:57:28 -0400

    arreglo de los paneles de editar y crear articulo

--- Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
+++ Proyecto Final 4 DPOO/src/Principal/Inicializadora.java
@@ -1863,10 +1863,10 @@ public class Inicializadora{
 				c4m5d3.emitirNota(d252, d295, 5);
 				c4m5d3.emitirNota(d252, d298, 3);
 
-				/*VentanaLogin v = new VentanaLogin(vicedecanato);	
-				v.setVisible(true);*/
-				VentanaPrincipal p = new VentanaPrincipal(vicedecanato);
-				new VentanaGestionDepartamento(p, depto1, vicedecanato);
+				VentanaLogin v = new VentanaLogin(vicedecanato);	
+				v.setVisible(true);
+				//VentanaPrincipal p = new VentanaPrincipal(vicedecanato);
+				//new VentanaGestionDepartamento(p, depto1, vicedecanato);
 			}
 		});
 	}
